2382d3d2e66e8b955d8380acbc9a8147c9db53ef
elisabichiappini <elisabichiappini@gmail.com>
2024-02-04 22:32:29 +0100

    feat: bg index pages

--- error_logica_html/index.php
+++ error_logica_html/index.php
@@ -30,10 +30,10 @@ if(!empty($_POST['mail'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <!--/CDN CSS Bootstrap-->
 </head>
-<body>
+<body class="bg-info-subtle pt-5">
     <!--intestazione-->
     <header>
-        <h1>Accedi SitoBello</h1>
+        <h1 class="text-center">Accedi SitoBello</h1>
     </header>
     <!-- /intestazione-->
     <!--corpo-->
